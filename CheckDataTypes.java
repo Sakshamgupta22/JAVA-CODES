351dab054d7c65d2e8769108ad904d8d537eaa21
Sakshamgupta22 <saksham.gupta_bca.ds25@gla.ac.in>
2026-01-30 20:45:30 +0530

    CODE

---
+++ CheckDataTypes.java
@@ -0,0 +1,20 @@
+public class CheckDataTypes{
+     public static void main(String[] args){
+           boolean b1 = true;
+           boolean b2 = false;
+           byte a = 12;
+           byte b = -23;
+           short num = 1500;
+           short t = -400; 
+           int population = 1002000;
+           int distance = 2000;
+           System.out.println("B1:"+b1);
+           System.out.println("B2"+ b2);
+           System.out.println("Age :" + a);
+           System.out.println("Temperature :" + b);
+           System.out.println("number :" + num);
+           System.out.println("Temperature :" + t);
+           System.out.println("Population :" + population);
+           System.out.println("Distance :" + distance);
+} 
+}
