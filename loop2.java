83d53a2072669fa18ebeb201e59d235dcd642457
Sakshamgupta22 <saksham.gupta_bca.ds25@gla.ac.in>
2026-02-10 12:50:57 +0530

    Adding

--- loop2.java
+++ loop2.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class loop2 {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    float area,l,b,r,side;
+    float pi = 3.14f;
     
     }
 }
