fa3b7fe099b33908756d2d214bd1e5ce083323b8
Sakshamgupta22 <saksham.gupta_bca.ds25@gla.ac.in>
2026-02-18 17:14:36 +0530

    Add Java File

---
+++ Methods.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Methods {
+     int empid,y_experience;
+     String empname;
+     float basic_salary,y_increment,bonus,gross_salary;
+     Scanner sc = new Scanner(System.in);
+     void Getdata(){
+     System.out.println("Enter employee id");
+     empid = sc.nextInt(); 
+     System.out.println("Enter employee name");
+     empname =  sc.next();
+     System.out.println("Enter employee basic salary");
+     basic_salary = sc.nextFloat();
+     System.out.println("Enter employee year experience");
+     y_experience = sc.nextInt();
+     }
+     float Gross_salary(){
+        if( y_experience%5==0){
+
+        bonus = (15/100f) * basic_salary;
+        }
+        else{
+            System.out.println("No bonus");
+        }
+        y_increment = (6/100f) * basic_salary;
+        gross_salary = basic_salary + bonus + y_increment;
+        return gross_salary;    
+     }  
+     void  DisplayData(){
+        System.out.println("Employee id "+empid);
+        System.out.println("Employee name "+empname);
+        System.out.println("Employee basic salary "+basic_salary);
+        System.out.println("Employee year experience "+y_experience);
+        System.out.println("Employee y_increment "+y_increment);
+        System.out.println("Employee bonus "+bonus);
+        System.out.println("Employee gross salary "+ gross_salary);
+     }
+     public static void main(String[] args){
+        Methods obj = new Methods();
+        obj.Getdata();
+        obj.Gross_salary();
+        obj.DisplayData();
+
+     }
+}
